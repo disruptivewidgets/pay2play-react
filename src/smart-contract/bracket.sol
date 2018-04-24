cca1d79e3f5c1d70aa4bd28be0dc5ee258665a04
Destinatis <accounts@playent.net>
2018-04-23 22:37:20 -0700

    sync

--- src/smart-contract/bracket.sol
+++ src/smart-contract/bracket.sol
@@ -6,7 +6,7 @@
 
 pragma solidity ^0.4.21;
 
-contract Match
+/* contract Match
 {
   struct match
   {
@@ -15,7 +15,7 @@ contract Match
       uint amount;
       address winner;
   }
-}
+} */
 
 // 1, 2, 3,  4,  5
 // 2, 4, 8, 16, 32
@@ -30,218 +30,249 @@ contract Match
 
 // number of nodes in a tree = n - 1
 
-contract Event
+contract Tournament
 {
   bool public active;
   uint public creationDate;
+  uint public numberOfParticipants;
 
   address public winner;
 
-  address constant burn = 0xdead;
+  address[2][] games_SideA;
+  address[2][] games_SideB;
 
-  struct match
-  {
-      address[] participants;
-      address winner;
-  }
+  address[] winners_SideA;
+  address[] winners_SideB;
 
-  match[] public matchesSideA;
-  match[] public matchesSideB;
+  struct range { uint start; uint finish; }
 
-  function Event(uint _numberOfParticipants) payable public
+  constructor(uint _numberOfParticipants) public
   {
     active = true;
     creationDate = now;
+    numberOfParticipants = _numberOfParticipants;
 
-    matchesSideA = new match[_numberOfParticipants - 1];
-    matchesSideB = new match[_numberOfParticipants - 1];
+    activate();
+  }
 
-    for (uint i = 0; i < _numberOfParticipants; i++)
+  function activate() public
+  {
+    uint i = 0;
+
+    for (i = 0; i < numberOfParticipants - 1; i++)
     {
-      matchesSideA[i] = match(new address[](2), burn);
-      matchesSideB[i] = match(new address[](2), burn);
+      games_SideA.push([address(0), address(0)]);
+      winners_SideA.push(address(0));
     }
 
-    if (_numberOfParticipants == 32)
+    for (i = 0; i < numberOfParticipants - 1; i++)
     {
-      //A
-      matchesSideA[4] = new match[](16);
-      matchesSideA[3] = new match[](8);
-      matchesSideA[2] = new match[](4);
-      matchesSideA[1] = new match[](2);
-      matchesSideA[0] = new match[](1);
-
-      //B
-      matchesSideB[4] = new match[](16);
-      matchesSideB[3] = new match[](8);
-      matchesSideB[2] = new match[](4);
-      matchesSideB[1] = new match[](2);
-      matchesSideB[0] = new match[](1);
-    }
-
-    if (_numberOfParticipants == 16)
-    {
-      //A
-      matchesSideA[3] = new match[](8);
-      matchesSideA[2] = new match[](4);
-      matchesSideA[1] = new match[](2);
-      matchesSideA[0] = new match[](1);
-
-      //B
-      matchesSideB[3] = new match[](8);
-      matchesSideB[2] = new match[](4);
-      matchesSideB[1] = new match[](2);
-      matchesSideB[0] = new match[](1);
-    }
-
-    if (_numberOfParticipants == 8)
-    {
-      //A
-      matchesSideA[2] = new match[](4);
-      matchesSideA[1] = new match[](2);
-      matchesSideA[0] = new match[](1);
-
-      //B
-      matchesSideB[2] = new match[](4);
-      matchesSideB[1] = new match[](2);
-      matchesSideB[0] = new match[](1);
-    }
-
-    if (_numberOfParticipants == 4)
-    {
-      //A
-      matchesSideA[1] = new match[](2);
-      matchesSideA[0] = new match[](1);
-
-      //B
-      matchesSideB[1] = new match[](2);
-      matchesSideB[0] = new match[](1);
-    }
-
-    if (_numberOfParticipants == 2)
-    {
-      //A
-      matchesSideA[0] = new match[](1);
-
-      //B
-      matchesSideA[0] = new match[](1);
+      games_SideB.push([address(0), address(0)]);
+      winners_SideB.push(address(0));
     }
   }
 
-  event Match_SideA_Player_Joined(uint indexed level, uint indexed index, uint indexed slot, address indexed player);
-  event Match_SideB_Player_Joined(uint indexed level, uint indexed index, uint indexed slot, address indexed player);
+  event Game_SideA_Player_Joined(uint indexed index, uint slot, address indexed player);
+  event Game_SideB_Player_Joined(uint indexed index, uint slot, address indexed player);
 
-  function joinMatch_SideA(uint index)
+  function joinGame_SideA(uint index, uint slot) public
   {
-    uint level = 4;
+    addPlayerToGame_SideA(index, slot, msg.sender);
+    emit Game_SideA_Player_Joined(index, slot, msg.sender);
+  }
+  function joinGame_SideB(uint index, uint slot) public
+  {
+    addPlayerToGame_SideB(index, slot, msg.sender);
+    emit Game_SideB_Player_Joined(index, slot, msg.sender);
+  }
 
-    if (_numberOfParticipants == 32)
+  function addPlayerToGame_SideA(uint index, uint slot, address player) public
+  {
+    if (games_SideA[slot][index] == address(0))
     {
-      level = 4;
-    }
-    if (_numberOfParticipants == 16)
-    {
-      level = 3;
-    }
-    if (_numberOfParticipants == 8)
-    {
-      level = 2;
-    }
-    if (_numberOfParticipants == 4)
-    {
-      level = 1;
-    }
-    if (_numberOfParticipants == 2)
-    {
-      level = 0;
-    }
-
-    address player = msg.sender;
-    match m = matchesSideA[level][index];
-
-    if (m.participants[0] == address(0))
-    {
-      m.participants.push(player);
-      emit Match_SideA_Player_Joined(level, index, 0, player);
-      return;
-    }
-
-    if (m.participants[1] == address(0))
-    {
-      m.participants.push(player);
-      emit Match_SideA_Player_Joined(level, index, 1, player);
-      return;
+      games_SideA[slot][index] = player;
     }
   }
-  function joinMatch_SideB(uint index)
+  function addPlayerToGame_SideB(uint index, uint slot, address player) public
   {
-    uint level = 4;
-
-    if (_numberOfParticipants == 32)
+    if (games_SideB[slot][index] == address(0))
     {
-      level = 4;
-    }
-    if (_numberOfParticipants == 16)
-    {
-      level = 3;
-    }
-    if (_numberOfParticipants == 8)
-    {
-      level = 2;
-    }
-    if (_numberOfParticipants == 4)
-    {
-      level = 1;
-    }
-    if (_numberOfParticipants == 2)
-    {
-      level = 0;
-    }
-
-    address player = msg.sender;
-    match m = matchesSideB[level][index];
-
-    if (m.participants[0] == address(0))
-    {
-      m.participants.push(player);
-      emit Match_SideB_Player_Joined(level, index, 0, player);
-      return;
-    }
-
-    if (m.participants[1] == address(0))
-    {
-      m.participants.push(player);
-      emit Match_SideB_Player_Joined(level, index, 1, player);
-      return;
+      games_SideB[slot][index] = player;
     }
   }
 
-  function getMatchWinner_SideA(uint level, uint index) constant public returns (address)
+  function getGameWinner_SideA(uint index) constant public returns (address)
   {
-    match m = matchesSideA[level][index];
-    return m.winner;
+    return winners_SideA[index];
   }
-  function getMatchWinner_SideB(uint level, uint index) constant public returns (address)
+  function getGameWinner_SideB(uint index) constant public returns (address)
   {
-    match m = matchesSideB[level][index];
-    return m.winner;
+    return winners_SideB[index];
   }
 
-  event MatchWinner_SideA_Updated(uint indexed level, uint indexed index, address indexed winner);
-  event MatchWinner_SideB_Updated(uint indexed level, uint indexed index, address indexed winner);
+  event GameWinner_SideA_Updated(uint indexed index, address indexed player);
+  event GameWinner_SideB_Updated(uint indexed index, address indexed player);
 
-  function setMatchWinner_SideA(uint level, uint index, address winner) onlyRegistrar public
+  function setGameWinner_SideA(uint index, address player) public
   {
-    match m = matchesSideA[level][index];
-    m.winner = winner;
+    winners_SideA[index] = player;
 
-    emit MatchWinner_SideA_Updated(level, index, winner);
+    uint start;
+    uint finish;
+
+    // find depth
+    (start, finish) = getDepthRange(index);
+
+    for (uint i = start; i < finish; i++)
+    {
+      if (games_SideA[0][i] == address(0))
+      {
+        addPlayerToGame_SideA(i, 0, player);
+      }
+
+      if (games_SideA[1][i] == address(0))
+      {
+        addPlayerToGame_SideA(i, 0, player);
+      }
+    }
+
+    emit GameWinner_SideA_Updated(index, player);
   }
-  function setMatchWinner_SideB(uint level, uint index, address winner) onlyRegistrar public
+  function setGameWinner_SideB(uint index, address player) public
   {
-    match m = matchesSideB[level][index];
-    m.winner = winner;
+    winners_SideB[index] = player;
 
-    emit MatchWinner_SideB_Updated(level, index, winner);
+    uint start;
+    uint finish;
+
+    // find depth
+    (start, finish) = getDepthRange(index);
+
+    for (uint i = start; i < finish; i++)
+    {
+      if (games_SideB[0][i] == address(0))
+      {
+        addPlayerToGame_SideB(i, 0, player);
+      }
+
+      if (games_SideB[1][i] == address(0))
+      {
+        addPlayerToGame_SideB(i, 0, player);
+      }
+    }
+
+    emit GameWinner_SideB_Updated(index, player);
+  }
+
+  function getDepthRange(uint index) constant public returns (uint, uint)
+  {
+    if (numberOfParticipants == 32)
+    {
+      // depth = 4
+      if (index >= 0 && index < 16)
+      {
+        return (16, 23);
+      }
+
+      if (index >= 16 && index < 24)
+      {
+        return (24, 28);
+      }
+
+      if (index >= 24 && index < 28)
+      {
+        return (28, 30);
+      }
+
+      if (index >= 28 && index < 30)
+      {
+        return (30, 31);
+      }
+
+      if (index >= 30 && index < 31)
+      {
+        return (31, 31);
+      }
+      //
+    }
+
+    if (numberOfParticipants == 16)
+    {
+      // depth = 3
+      if (index >= 0 && index < 8)
+      {
+        return (8, 12);
+      }
+
+      if (index >= 8 && index < 12)
+      {
+        return (12, 14);
+      }
+
+      if (index >= 12 && index < 14)
+      {
+        return (14, 15);
+      }
+
+      if (index >= 14 && index < 15)
+      {
+        return (15, 15);
+      }
+      //
+    }
+
+    if (numberOfParticipants == 8)
+    {
+      // depth 2
+      if (index >= 0 && index < 4)
+      {
+        return (4, 6);
+      }
+
+      if (index >= 4 && index < 6)
+      {
+        return (6, 7);
+      }
+
+      if (index >= 6 && index < 7)
+      {
+        return (7, 7);
+      }
+      //
+    }
+
+    if (numberOfParticipants == 4)
+    {
+      // depth 1
+      if (index >= 0 && index < 2)
+      {
+        return (2, 3);
+      }
+
+      if (index >= 2 && index < 3)
+      {
+        return (3, 3);
+      }
+      //
+    }
+
+    if (numberOfParticipants == 2)
+    {
+      if (index >= 0 && index < 1)
+      {
+        return (1, 1);
+      }
+    }
+
+    return (0, 0);
+  }
+
+  function getGame_SideA(uint index) constant public returns (address, address)
+  {
+    return (games_SideA[0][index], games_SideA[1][index]);
+  }
+  function getGame_SideB(uint index) constant public returns (address, address)
+  {
+    return (games_SideB[0][index], games_SideB[1][index]);
   }
 }
